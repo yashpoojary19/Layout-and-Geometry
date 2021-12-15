4789621743ff9eec790a477a25a3c3730ce1da39
Yash Poojary <yashspoojary245@gmail.com>
2021-12-15 13:23:41 +0530

    Lecture completed

--- Layout and Geometry/ContentView.swift
+++ Layout and Geometry/ContentView.swift
@@ -8,32 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
-
+    
+    
     var body: some View {
-        VStack {
-            Spacer()
-        GeometryReader { fullView in
+        GeometryReader { fullview in
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack {
                     ForEach(0..<50) { index in
                         GeometryReader { geo in
                             Rectangle()
-                                .fill(self.colors[index % 7])
+                                .fill(colors[index % 7])
                                 .frame(height: 150)
-                                .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).midX - fullView.size.width / 2) / 10), axis: (x: 0, y: 1, z: 0))
+                                .rotation3DEffect(.degrees(Double(geo.frame(in: .global).midX - fullview.size.width / 2) / 10), axis: (x: 0, y: 1, z:0))
                         }
                         .frame(width: 150)
                     }
                 }
-                .padding(.horizontal, (fullView.size.width - 150) / 2)
+                .padding(.horizontal, (fullview.size.width - 150) / 2)
             }
+            
         }
-        .ignoresSafeArea()
-    }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -41,6 +39,35 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
     
+
+
+//struct ContentView: View {
+//    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+//
+//    var body: some View {
+//        VStack {
+//            Spacer()
+//        GeometryReader { fullView in
+//            ScrollView(.horizontal, showsIndicators: false) {
+//                HStack {
+//                    ForEach(0..<50) { index in
+//                        GeometryReader { geo in
+//                            Rectangle()
+//                                .fill(self.colors[index % 7])
+//                                .frame(height: 150)
+//                                .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).midX - fullView.size.width / 2) / 10), axis: (x: 0, y: 1, z: 0))
+//                        }
+//                        .frame(width: 150)
+//                    }
+//                }
+//                .padding(.horizontal, (fullView.size.width - 150) / 2)
+//            }
+//        }
+//        .ignoresSafeArea()
+//    }
+//    }
+//}
+//
 
 //
 
